f07139d551784aba56a2e7e5e06b1fec35f109e3
raoulmeli <meliraoul23@gmail.com>
2026-04-03 22:54:49 +0200

    Create index-Bjjdu1Cb.js for static assets
    Added a large JavaScript file for static assets.

---
+++ static/static/static/assets/static/assets/index-Bjjdu1Cb.js
@@ -0,0 +1,12 @@
+Excellent ! 👍 **Dernier fichier !**
+
+---
+
+## 📄 FICHIER 8 : `static/assets/index-Bjjdu1Cb.js`
+
+1. Cliquez **"Add file"** → **"Create new file"**
+2. Nom : **`static/assets/index-Bjjdu1Cb.js`**
+3. Ce fichier est TRÈS GROS. Je vais vous le donner en parties.
+Action: $ wc -c /app/bloc-manager-complet/static/assets/index-Bjjdu1Cb.js
+Observation: 738694 /app/bloc-manager-complet/static/assets/index-Bjjdu1Cb.js
+Exit code: 0
